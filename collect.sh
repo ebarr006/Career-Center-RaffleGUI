dd292b71602f5ff8c7b3e6c7d907f6c1f61b086c
Emilio Barreiro <ebarr006@ucr.edu>
2016-11-24 22:46:21 -0800

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -1,3 +1,7 @@
-rm output.txt
+if [ -f /Users/emiliobarreiro/UCR/CareerCenterApp/output.txt ]; then
+	rm output.txt
+fi
+# making sure a clean text file is present for new raffle winner's name and info
+
 touch output.txt
 python raffle.py > output.txt
